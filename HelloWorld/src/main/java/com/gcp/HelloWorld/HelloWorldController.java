2a6219c4525a9b8ad66b5491e8b1c862002ca770
amit dubey <amit.leosoft@gmail.com>
2026-01-15 15:24:19 +0530

    Initial commit

---
+++ HelloWorld/src/main/java/com/gcp/HelloWorld/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.gcp.HelloWorld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+
+    // Mapping the root URL ("/") to this method
+    @RequestMapping("/")
+    public String helloWorld() {
+
+        // Returning a simple "Hello World" response
+        return "Hello World";
+    }
+}
+
+
+
